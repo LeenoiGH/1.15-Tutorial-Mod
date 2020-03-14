ca1ea10e20670b2d8041d64bf6b9cd77c7c123b4
LeenoiGH <61032443+LeenoiGH@users.noreply.github.com>
2020-03-14 13:39:21 -0700

    ItemGroups and updated mappings

--- src/main/java/com/leenoimc/tutorialmod/init/BlockInit.java
+++ src/main/java/com/leenoimc/tutorialmod/init/BlockInit.java
@@ -1,13 +1,13 @@
 package com.leenoimc.tutorialmod.init;
 
 import com.leenoimc.tutorialmod.TutorialMod;
+import com.leenoimc.tutorialmod.TutorialMod.TutorialItemGroup;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -31,7 +31,7 @@ public class BlockInit
 	@SubscribeEvent()
 	public static void registerBlockItems(final RegistryEvent.Register<Item> event)
 	{
-		event.getRegistry().register(new BlockItem(tutorial_block, new Item.Properties().group(ItemGroup.DECORATIONS))
+		event.getRegistry().register(new BlockItem(tutorial_block, new Item.Properties().group(TutorialItemGroup.instance))
 				.setRegistryName("tutorial_block"));
 	}
 }
--- src/main/java/com/leenoimc/tutorialmod/init/ItemInit.java
+++ src/main/java/com/leenoimc/tutorialmod/init/ItemInit.java
@@ -1,10 +1,10 @@
 package com.leenoimc.tutorialmod.init;
 
 import com.leenoimc.tutorialmod.TutorialMod;
+import com.leenoimc.tutorialmod.TutorialMod.TutorialItemGroup;
 
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraftforge.event.RegistryEvent;
@@ -22,10 +22,10 @@ public class ItemInit {
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
 		event.getRegistry()
-				.register(new Item(new Item.Properties().group(ItemGroup.MISC)).setRegistryName("tutorial_item"));
+				.register(new Item(new Item.Properties().group(TutorialItemGroup.instance)).setRegistryName("tutorial_item"));
 
 		event.getRegistry()
-				.register(new Item(new Item.Properties().group(ItemGroup.FOOD)
+				.register(new Item(new Item.Properties().group(TutorialItemGroup.instance)
 						.food(new Food.Builder().hunger(20).saturation(40.0F)
 								.effect(new EffectInstance(Effects.GLOWING, 60, 3), 6.0F).build()))
 										.setRegistryName("tutorial_food"));
