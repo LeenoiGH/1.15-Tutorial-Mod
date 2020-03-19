72dc2d70f03413061c3b658426a7cf0b41785270
LeenoiGH <61032443+LeenoiGH@users.noreply.github.com>
2020-03-18 20:45:34 -0700

    Custom Block Models
    This took a while to make as you can see, please have some sympathy ;(

--- src/main/java/com/leenoimc/tutorialmod/init/BlockInit.java
+++ src/main/java/com/leenoimc/tutorialmod/init/BlockInit.java
@@ -2,6 +2,7 @@ package com.leenoimc.tutorialmod.init;
 
 import com.leenoimc.tutorialmod.TutorialMod;
 import com.leenoimc.tutorialmod.TutorialMod.TutorialItemGroup;
+import com.leenoimc.tutorialmod.objects.blocks.SpecialBlock;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
@@ -17,21 +18,27 @@ import net.minecraftforge.registries.ObjectHolder;
 
 @ObjectHolder(TutorialMod.MOD_ID)
 @Mod.EventBusSubscriber(modid = TutorialMod.MOD_ID, bus = Bus.MOD)
-public class BlockInit
-{
+public class BlockInit {
 	public static final Block tutorial_block = null;
-	
+	public static final Block special_block = null;
+
 	@SubscribeEvent
-	public static void registerBlocks(final RegistryEvent.Register<Block> event)
-	{
-		event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(5.0F,3.0F)
+	public static void registerBlocks(final RegistryEvent.Register<Block> event) {
+		event.getRegistry().register(new Block(Block.Properties.create(Material.ROCK).hardnessAndResistance(5.0F, 3.0F)
 				.sound(SoundType.STONE).harvestTool(ToolType.PICKAXE)).setRegistryName("tutorial_block"));
+		event.getRegistry()
+				.register(new SpecialBlock(Block.Properties.create(Material.WOOD).hardnessAndResistance(0.5F, 15.0F)
+						.harvestLevel(2).harvestTool(ToolType.AXE).sound(SoundType.ANVIL))
+								.setRegistryName("special_block"));
 	}
-	
+
 	@SubscribeEvent()
-	public static void registerBlockItems(final RegistryEvent.Register<Item> event)
-	{
-		event.getRegistry().register(new BlockItem(tutorial_block, new Item.Properties().group(TutorialItemGroup.instance))
-				.setRegistryName("tutorial_block"));
+	public static void registerBlockItems(final RegistryEvent.Register<Item> event) {
+		event.getRegistry()
+				.register(new BlockItem(tutorial_block, new Item.Properties().group(TutorialItemGroup.instance))
+						.setRegistryName("tutorial_block"));
+		event.getRegistry()
+		.register(new BlockItem(special_block, new Item.Properties().group(TutorialItemGroup.instance))
+				.setRegistryName("special_block"));
 	}
 }
--- src/main/java/com/leenoimc/tutorialmod/init/ItemInit.java
+++ src/main/java/com/leenoimc/tutorialmod/init/ItemInit.java
@@ -6,9 +6,12 @@ import com.leenoimc.tutorialmod.TutorialMod;
 import com.leenoimc.tutorialmod.TutorialMod.TutorialItemGroup;
 import com.leenoimc.tutorialmod.objects.items.SpecialItem;
 
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.ArmorItem;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.Food;
 import net.minecraft.item.HoeItem;
+import net.minecraft.item.IArmorMaterial;
 import net.minecraft.item.IItemTier;
 import net.minecraft.item.Item;
 import net.minecraft.item.PickaxeItem;
@@ -18,6 +21,10 @@ import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.util.LazyValue;
+import net.minecraft.util.SoundEvent;
+import net.minecraft.util.SoundEvents;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -29,6 +36,7 @@ import net.minecraftforge.registries.ObjectHolder;
 public class ItemInit {
 	public static final Item tutorial_item = null;
 	public static final Item tutorial_food = null;
+	public static final Item special_item = null;
 
 	// Tools
 	public static final Item tutorial_pickaxe = null;
@@ -37,6 +45,12 @@ public class ItemInit {
 	public static final Item tutorial_shovel = null;
 	public static final Item tutorial_hoe = null;
 
+	// Armour
+	public static final Item tutorial_helmet = null;
+	public static final Item tutorial_chestplate = null;
+	public static final Item tutorial_leggings = null;
+	public static final Item tutorial_boots = null;
+
 	@SubscribeEvent
 	public static void registerItems(final RegistryEvent.Register<Item> event) {
 		event.getRegistry().register(
@@ -44,14 +58,14 @@ public class ItemInit {
 
 		event.getRegistry().register(new SpecialItem(new Item.Properties().group(TutorialItemGroup.instance))
 				.setRegistryName("special_item"));
-		
+
 		// Food
 		event.getRegistry()
 				.register(new Item(new Item.Properties().group(TutorialItemGroup.instance)
 						.food(new Food.Builder().hunger(20).saturation(40.0F)
 								.effect(new EffectInstance(Effects.GLOWING, 60, 3), 6.0F).build()))
 										.setRegistryName("tutorial_food"));
-		
+
 		// Tools
 		event.getRegistry().register(new PickaxeItem(ModItemTier.TUTORIAL, 10, 10.7F,
 				new Item.Properties().group(TutorialItemGroup.instance)).setRegistryName("tutorial_pickaxe"));
@@ -67,6 +81,16 @@ public class ItemInit {
 		event.getRegistry().register(
 				new HoeItem(ModItemTier.TUTORIAL, 8.0F, new Item.Properties().group(TutorialItemGroup.instance))
 						.setRegistryName("tutorial_hoe"));
+
+		// Armour
+		event.getRegistry().register(new ArmorItem(ModArmorMaterial.TEST, EquipmentSlotType.HEAD,
+				new Item.Properties().group(TutorialItemGroup.instance)).setRegistryName("tutorial_helmet"));
+		event.getRegistry().register(new ArmorItem(ModArmorMaterial.TEST, EquipmentSlotType.CHEST,
+				new Item.Properties().group(TutorialItemGroup.instance)).setRegistryName("tutorial_chestplate"));
+		event.getRegistry().register(new ArmorItem(ModArmorMaterial.TEST, EquipmentSlotType.LEGS,
+				new Item.Properties().group(TutorialItemGroup.instance)).setRegistryName("tutorial_leggings"));
+		event.getRegistry().register(new ArmorItem(ModArmorMaterial.TEST, EquipmentSlotType.FEET,
+				new Item.Properties().group(TutorialItemGroup.instance)).setRegistryName("tutorial_boots"));
 	}
 
 	public enum ModItemTier implements IItemTier {
@@ -119,6 +143,71 @@ public class ItemInit {
 		@Override
 		public Ingredient getRepairMaterial() {
 			return this.repairMaterial.getValue();
+		}
+	}
+
+	public enum ModArmorMaterial implements IArmorMaterial {
+		TEST(TutorialMod.MOD_ID + ":test", 5, new int[] { 50, 70, 110, 50 }, 420, SoundEvents.field_226124_Y_, 6.9F, () -> {
+			return Ingredient.fromItems(ItemInit.tutorial_item); // For some reason eclipse crashes when i edit this
+																	// ItemInit part. If you're getting the same problem
+																	// please edit outside of eclipse
+		});
+
+		private static final int[] MAX_DAMAGE_ARRAY = new int[] { 50, 50, 50, 50 };
+		private final String name;
+		private final int maxDamageFactor;
+		private final int[] damageReductionAmountArray;
+		private final int enchantability;
+		private final SoundEvent soundEvent;
+		private final float toughness;
+		private final LazyValue<Ingredient> repairMaterial;
+
+		private ModArmorMaterial(String nameIn, int maxDamageFactorIn, int[] damageReductionAmountIn,
+				int enchantabilityIn, SoundEvent soundEventIn, float toughnessIn,
+				Supplier<Ingredient> repairMaterialIn) {
+			this.name = nameIn;
+			this.maxDamageFactor = maxDamageFactorIn;
+			this.damageReductionAmountArray = damageReductionAmountIn;
+			this.enchantability = enchantabilityIn;
+			this.soundEvent = soundEventIn;
+			this.toughness = toughnessIn;
+			this.repairMaterial = new LazyValue<>(repairMaterialIn);
+		}
+
+		@Override
+		public int getDurability(EquipmentSlotType slotIn) {
+			return MAX_DAMAGE_ARRAY[slotIn.getIndex()] * this.maxDamageFactor;
+		}
+
+		@Override
+		public int getDamageReductionAmount(EquipmentSlotType slotIn) {
+			return this.damageReductionAmountArray[slotIn.getIndex()];
+		}
+
+		@Override
+		public int getEnchantability() {
+			return this.enchantability;
+		}
+
+		@Override
+		public SoundEvent getSoundEvent() {
+			return this.soundEvent;
+		}
+
+		@Override
+		public Ingredient getRepairMaterial() {
+			return this.repairMaterial.getValue();
+		}
+
+		@OnlyIn(Dist.CLIENT)
+		@Override
+		public String getName() {
+			return this.name;
+		}
+
+		@Override
+		public float getToughness() {
+			return this.toughness;
 		}
 	}
 }
